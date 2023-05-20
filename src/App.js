f427f64449a3876e73f6771ed22990da1c9e0f9b
noorkaurr <noorkaur@uw.edu>
2023-05-20 15:08:08 -0700

    added basic App.js structure

--- src/App.js
+++ src/App.js
@@ -1,1 +1,57 @@
+import React from "react";
 
+function App() {
+    return (
+        <div>
+             {/* <Nav /> */}
+             
+             {/* <RecipeFeed /> */}
+             <section className="RecipeFeed">
+                <h2 className="recipe-feed">Recipe Feed</h2>
+                <form>
+                    <div className="specification-sections">
+                        <div className="cost-section">
+                            {/* <CostFilter /> */}
+                        </div>
+                        <div className="time-section">
+                            {/* <TimeFilter /> */}
+                        </div>
+                        <div className="diet-section">
+                            {/* <DietFilter */}
+                        </div>
+                        <div className="allergy-section">
+                            {/* AllergyFilter */}
+                        </div>
+                        <div className="nutrition-section">
+                            {/* NutritionFilter */}
+                        </div>
+                    </div>
+                </form>
+
+                <div className="container">
+                    <div className="row">
+                        {/* Post 1 */}
+                        {/* <RecipeCard /> */}
+
+                        {/* Post 2 */}
+                        {/* <RecipeCard /> */}
+
+                        {/* Post 3 */}
+                        {/* <RecipeCard /> */}
+
+                        {/* Post 4 */}
+                        {/* <RecipeCard /> */}
+
+                    </div>
+                </div>
+             </section>
+             <footer>
+                <div class="container">
+                    <p><a href="mailto:email@collegemeals.uw.edu"><span class="material-icons"></span> email@collegemeals.uw.edu</a></p>
+                    <p><a href="tel:555-555-5555"><span class="material-icons">phone</span> 555-555-5555</a></p>
+                    <p>&copy; INFO 340 Group 6, The Noun Project, iStock </p>
+                </div>
+             </footer>
+        </div>
+    );
+}

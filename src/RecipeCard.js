fcf7567aee7f5641a65d846ed9ad5d1d4772e8e3
noorkaurr <noorkaur@uw.edu>
2023-05-21 16:02:50 -0700

    added recipe card component

---
+++ src/RecipeCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function RecipeCard({ recipe }) {
+  return (
+    <div className="col-12 col-md-6">
+      <div className="card">
+        <div className="card-header">
+          <h3 className="card-title">{recipe.title}</h3>
+        </div>
+        <img src={recipe.img} className="card-img-top" alt="Recipe" />
+        <div className="card-body">
+          <p className="card-text">{recipe.description}</p>
+        </div>
+        <div className="card-footer">
+          <span className="star-filled">&#9733;</span>
+          <span className="star-filled">&#9733;</span>
+          <span className="star-filled">&#9733;</span>
+          <span className="star-filled">&#9733;</span>
+          <span className="star">&#9733;</span>
+          <small className="text-muted">Posted by {recipe.author}</small>
+          <button type="button" className="btn btn-secondary">
+            View More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export { RecipeCard };
--- src/RecipeFeed.js
+++ src/RecipeFeed.js
@@ -4,97 +4,99 @@ import { TimeFilter } from "./TimeFilter";
 import { DietFilter } from "./DietFilter";
 import { AllergyFilter } from "./AllergyFilter";
 import { NutritionFilter } from "./NutritionFilter";
+import { RecipeCard } from "./RecipeCard";
 
 function RecipeFeed(props) {
-    // const recipes = props.recipes;
-    const onFilter = props.onFilter;
-    const [selectedOptions, setSelectedOptions] = useState({
-        cost: "",
-        time: "",
-        diet: "",
-        allergies: [],
-        nutrition: ""
-    });
+  const recipes = props.recipes;
+  const onFilter = props.onFilter;
+  const [selectedOptions, setSelectedOptions] = useState({
+    cost: "",
+    time: "",
+    diet: "",
+    allergies: [],
+    nutrition: ""
+  });
 
-    useEffect(() => {
-        onFilter(selectedOptions);
-    }, [selectedOptions, selectedOptions.time]);
+  useEffect(() => {
+    onFilter(selectedOptions);
+  }, [selectedOptions, selectedOptions.time]);
 
-    // const [selectedCost, setSelectedCost] = useState("");
-    const handleCostChange = (cost) => {
-        setSelectedOptions((prevOptions) => ({
-            ...prevOptions,
-            cost: cost,
-        }));
-    };
+  const handleCostChange = (cost) => {
+    setSelectedOptions((prevOptions) => ({
+      ...prevOptions,
+      cost: cost,
+    }));
+  };
 
-    const handleTimeChange = (time) => {
-        setSelectedOptions((prevOptions) => ({
-            ...prevOptions,
-            time: time,
-        }));
-    };
+  const handleTimeChange = (time) => {
+    setSelectedOptions((prevOptions) => ({
+      ...prevOptions,
+      time: time,
+    }));
+  };
 
-    const handleDietChange = (diet) => {
-        setSelectedOptions((prevOptions) => ({
-            ...prevOptions,
-            diet: diet,
-        }));
-    };
+  const handleDietChange = (diet) => {
+    setSelectedOptions((prevOptions) => ({
+      ...prevOptions,
+      diet: diet,
+    }));
+  };
 
-    const handleAllergyAdd = (allergy) => {
-        setSelectedOptions((prevOptions) => ({
-            ...prevOptions,
-            allergies: [...prevOptions.allergies, allergy],
-        }));
-    };
+  const handleAllergyAdd = (allergy) => {
+    setSelectedOptions((prevOptions) => ({
+      ...prevOptions,
+      allergies: [...prevOptions.allergies, allergy],
+    }));
+  };
 
-    const handleAllergyRemove = (allergy) => {
-        setSelectedOptions((prevOptions) => ({
-            ...prevOptions,
-            allergies: prevOptions.allergies.filter((a) => a !== allergy),
-        }));
-    };
+  const handleAllergyRemove = (allergy) => {
+    setSelectedOptions((prevOptions) => ({
+      ...prevOptions,
+      allergies: prevOptions.allergies.filter((a) => a !== allergy),
+    }));
+  };
 
-    const handleNutritionChange = (nutrition) => {
-        setSelectedOptions((prevOptions) => ({
-            ...prevOptions,
-            nutrition: nutrition,
-        }));
-    };
+  const handleNutritionChange = (nutrition) => {
+    setSelectedOptions((prevOptions) => ({
+      ...prevOptions,
+      nutrition: nutrition,
+    }));
+  };
 
-    return (
-        <section className="RecipeFeed">
-            <h2 className="recipe-feed">Recipe Feed</h2>
-            <form>
-                <div className="specification-sections">
-                    <div className="cost-section">
-                        <CostFilter selectedCost={selectedOptions.cost} onCostChange={handleCostChange} />
-                    </div>
-                    <div className="time-section">
-                        <TimeFilter onTimeChange={handleTimeChange}/>
-                    </div>
-                    <div className="diet-section">
-                        <DietFilter selectedDiet={selectedOptions.diet} onDietChange={handleDietChange}/>
-                    </div>
-                    <div className="allergy-section">
-                        <AllergyFilter onAllergyAdd={handleAllergyAdd} onAllergyRemove={handleAllergyRemove}/>
-                    </div>
-                    <div className="nutrition-section">
-                        <NutritionFilter onNutritionChange={handleNutritionChange}/>
-                    </div>
-
-                    <div className="container">
-                        <div className="row">
-                            {/* {recipes.map(recipe => (
-                                <RecipeCard key={recipe.id} recipe={recipe} />
-                            ))} */}
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </section>
-    );
+  return (
+    <section className="RecipeFeed">
+      <h2>Recipe Feed</h2>
+      <form className="filters">
+        <div className="filter-group">
+          <CostFilter
+            selectedCost={selectedOptions.cost}
+            onCostChange={handleCostChange}
+          />
+        </div>
+        <div className="filter-group">
+          <TimeFilter onTimeChange={handleTimeChange} />
+        </div>
+        <div className="filter-group">
+          <DietFilter onDietChange={handleDietChange} />
+        </div>
+        <div className="filter-group">
+          <AllergyFilter
+            allergies={selectedOptions.allergies}
+            onAllergyAdd={handleAllergyAdd}
+            onAllergyRemove={handleAllergyRemove}
+          />
+        </div>
+        <div className="filter-group">
+          <NutritionFilter onNutritionChange={handleNutritionChange} />
+        </div>
+      </form>
+      <div className="recipe-list">
+        {recipes.map((recipe) => (
+          <RecipeCard key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export { RecipeFeed };
